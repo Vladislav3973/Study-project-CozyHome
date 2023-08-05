9098c663cdbdcd00ed690a61b909d9b9c7cb6a1f
Vlad Pichuev <59628677+Vladislav3973@users.noreply.github.com>
2023-08-05 21:12:33 +0400

    mobile-first layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,8 @@
- 
+const burgerButton = document.querySelector(".burger__button");
+const activeButton = document.querySelector(".burger__list")
+const rotateStick = document.querySelector(".burger");
+
+burgerButton.onclick = function() {
+    activeButton.classList.toggle('active')
+    rotateStick.classList.toggle('active')
+};
