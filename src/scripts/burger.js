c0137a0f61705f7996d08d7ab208e4fce3562993
Vlad Pichuev <59628677+Vladislav3973@users.noreply.github.com>
2023-12-29 18:50:13 +0400

    error correction

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,5 +1,5 @@
 const burgerButton = document.querySelector(".burger__button");
-const activeButton = document.querySelector(".mobile_navigation")
+const activeButton = document.querySelector(".mobile__navigation")
 const firstStick = document.querySelector(".first_stick");
 const secondStick = document.querySelector(".second_stick");
 const thirdStick = document.querySelector(".third_stick");
